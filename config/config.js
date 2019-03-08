623981758218a342a502a115fe8ada278944ea85
Stefan Kashner <s.kash@yahoo.com>
2019-03-07 21:04:12 -0500

    updated config, added db folder

---
+++ config/config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  'config': path.resolve('config', 'config.js')
+} 
+
+module.exports = {
+  development: {
+    username: "root",
+    // make sure to set this env variable in your .env file
+    password: process.env.LOCAL_MYSQL_PASSWORD,
+    database: "blocks_db",
+    host: "localhost",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    /* eslint-disable-next-line camelcase */
+    use_env_variable: JAWSDB_URL,
+    dialect: mysql
+  }
+};
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,77 +1,79 @@
+USE blocks_db;
+
 INSERT INTO blocks (name, description, zone, lat1, lng1, lat2, lng2, spotsAvailable)
 
 -- Vertical Streets --
 
-VALUES ("blockOne", "S Juniper St - Pine > Lombard", 4, 39.9454, -75.16396, 39.94457, -75.16414, false)
-VALUES ("blockTwo", "S Juniper St - Lombard > South", 4, 39.4448, -75.16416, 39.94355, -75.16436, false)
+VALUES ("blockOne", "S Juniper St - Pine > Lombard", 4, 39.9454, -75.16396, 39.94457, -75.16414, false),
+       ("blockTwo", "S Juniper St - Lombard > South", 4, 39.4448, -75.16416, 39.94355, -75.16436, false),
 
-VALUES ("blockThree", "S 13th St - South > Lombard", 4, 39.94341, -75.16335, 39.94436, -75.16315, false)
-VALUES ("blockFour", "S 13th St - Lombard > Pine", 4, 39.94444, -75.16313, 39.94527, -75.16294, false)
-VALUES ("blockFive", "S 13th St - Pine > Spruce", 4, 39.94533, -75.16293, 39.94669, -75.16263, false)
-VALUES ("blockSix", "S 13th St - Spruce > Locust", 4, 39.94674, -75.16262, 39.9479, -75.16237, false)
+       ("blockThree", "S 13th St - South > Lombard", 4, 39.94341, -75.16335, 39.94436, -75.16315, false),
+       ("blockFour", "S 13th St - Lombard > Pine", 4, 39.94444, -75.16313, 39.94527, -75.16294, false),
+       ("blockFive", "S 13th St - Pine > Spruce", 4, 39.94533, -75.16293, 39.94669, -75.16263, false),
+       ("blockSix", "S 13th St - Spruce > Locust", 4, 39.94674, -75.16262, 39.9479, -75.16237, false),
 
-VALUES ("blockSeven", "S Camac St - Pine > Spruce", 4, 39.94658, -75.16185, 39.94523, -75.16215, false)
+       ("blockSeven", "S Camac St - Pine > Spruce", 4, 39.94658, -75.16185, 39.94523, -75.16215, false),
 
-VALUES ("blockEight", "S 12th St - Spruce > Pine", 4, 39.94648, -75.16104, 39.94512, -75.16133, false)
-VALUES ("blockNine", "S 12th St - Pine > Lombard", 4, 39.94506, -75.16136, 39.94425, -75.16153, false)
-VALUES ("blockTen", "S 12th St - Lombard > South", 4, 39.94416, -75.16155, 39.94323, -75.16176, false)
+       ("blockEight", "S 12th St - Spruce > Pine", 4, 39.94648, -75.16104, 39.94512, -75.16133, false),
+       ("blockNine", "S 12th St - Pine > Lombard", 4, 39.94506, -75.16136, 39.94425, -75.16153, false),
+       ("blockTen", "S 12th St - Lombard > South", 4, 39.94416, -75.16155, 39.94323, -75.16176, false),
 
-VALUES ("blockEleven", "S 11th St - South > Lombard", 4, 39.94303, -75.16019, 39.94396, -75.15999, false)
-VALUES ("blockTwelve", "S 11th St - Lombard > Pine", 4, 39.94406, -75.15997, 39.94487, -75.15979, false)
-VALUES ("blockThirteen", "S 11th St - Pine > Spruce", 4, 39.94493, -75.15978, 39.94629, -75.15948, false)
+       ("blockEleven", "S 11th St - South > Lombard", 4, 39.94303, -75.16019, 39.94396, -75.15999, false),
+       ("blockTwelve", "S 11th St - Lombard > Pine", 4, 39.94406, -75.15997, 39.94487, -75.15979, false),
+       ("blockThirteen", "S 11th St - Pine > Spruce", 4, 39.94493, -75.15978, 39.94629, -75.15948, false),
 
-VALUES ("blockFourteen", "S 10th St - Spruce > Pine", 4, 39.94609, -75.15791, 39.94474, -75.1582, false)
-VALUES ("blockFifteen", "S 10th St - Pine > Lombard", 4, 39.94467, -75.15822, 39.94386, -75.15839, false)
-VALUES ("blockSixteen", "S 10th St - Lombard > South", 4, 39.94376, -75.15841, 39.94284, -75.15862, false)
+       ("blockFourteen", "S 10th St - Spruce > Pine", 4, 39.94609, -75.15791, 39.94474, -75.1582, false),
+       ("blockFifteen", "S 10th St - Pine > Lombard", 4, 39.94467, -75.15822, 39.94386, -75.15839, false),
+       ("blockSixteen", "S 10th St - Lombard > South", 4, 39.94376, -75.15841, 39.94284, -75.15862, false),
 
-VALUES ("blockSeventeen", "S 9th St - South > Lombard", 4, 39.94264, -75.15706, 39.94357, -75.15685, false)
-VALUES ("blockEighteen", "S 9th St - Lombard > Pine", 4, 39.94367, -75.15684, 39.94447, -75.15665, false)
-VALUES ("blockNineteen", "S 9th St - Spruce > Locust", 4, 39.94596, -75.15633, 39.94711, -75.15608, false)
-VALUES ("blockTwenty", "S 9th St - Locust > Walnut", 4, 39.94721, -75.15607, 39.94825, -75.15585, false)
+       ("blockSeventeen", "S 9th St - South > Lombard", 4, 39.94264, -75.15706, 39.94357, -75.15685, false),
+       ("blockEighteen", "S 9th St - Lombard > Pine", 4, 39.94367, -75.15684, 39.94447, -75.15665, false),
+       ("blockNineteen", "S 9th St - Spruce > Locust", 4, 39.94596, -75.15633, 39.94711, -75.15608, false),
+       ("blockTwenty", "S 9th St - Locust > Walnut", 4, 39.94721, -75.15607, 39.94825, -75.15585, false),
 
-VALUES ("blockTwentyOne", "S 8th St - Pine > Lombard", 4, 39.94428, -75.15507, 39.94347, -75.15525, false)
-VALUES ("blockTwentyTwo", "S 8th St - Lombard > South", 4, 39.94337, -75.15526, 39.94245, -75.15548, false)
+       ("blockTwentyOne", "S 8th St - Pine > Lombard", 4, 39.94428, -75.15507, 39.94347, -75.15525, false),
+       ("blockTwentyTwo", "S 8th St - Lombard > South", 4, 39.94337, -75.15526, 39.94245, -75.15548, false),
 
 -- Horizontal Streets --
 
-VALUES ("blockTwentyThree", "Locust St - 10th > 9th", 4, 39.94735, -75.15758, 39.94717, -75.15614, false)
-VALUES ("blockTwentyFour", "Locust St - 9th > 8th", 4, 39.94615, -75.15602, 39.94697, -75.15454, false)
+       ("blockTwentyThree", "Locust St - 10th > 9th", 4, 39.94735, -75.15758, 39.94717, -75.15614, false),
+       ("blockTwentyFour", "Locust St - 9th > 8th", 4, 39.94615, -75.15602, 39.94697, -75.15454, false),
 
-VALUES ("blockTwentyFive", "Irving St - 11th > 10th", 4, 39.94691, -75.15928, 39.94673, -75.15784, false)
+       ("blockTwentyFive", "Irving St - 11th > 10th", 4, 39.94691, -75.15928, 39.94673, -75.15784, false),
 
-VALUES ("blockTwentySix", "Spruce St - 7th > 8th", 4, 39.94554, -75.15324, 39.94562, -75.1547, false)
-VALUES ("blockTwentySeven", "Spruce St - 9th > 10th", 4, 39.94593, -75.1564, 39.94612, -75.15784, false)
-VALUES ("blockTwentyEight", "Spruce St - 10th > 11th", 4, 39.94613, -75.15796, 39.94631, -75.15942, false)
-VALUES ("blockTwentyNine", "Spruce St - 11th > 12th", 4, 39.94632, -75.15954, 39.9465, -75.16098, false)
-VALUES ("blockThirty", "Spruce St - 12th > 13th", 4, 39.94652, -75.1611, 39.94671, -75.16259, false)
+       ("blockTwentySix", "Spruce St - 7th > 8th", 4, 39.94554, -75.15324, 39.94562, -75.1547, false),
+       ("blockTwentySeven", "Spruce St - 9th > 10th", 4, 39.94593, -75.1564, 39.94612, -75.15784, false),
+       ("blockTwentyEight", "Spruce St - 10th > 11th", 4, 39.94613, -75.15796, 39.94631, -75.15942, false),
+       ("blockTwentyNine", "Spruce St - 11th > 12th", 4, 39.94632, -75.15954, 39.9465, -75.16098, false),
+       ("blockThirty", "Spruce St - 12th > 13th", 4, 39.94652, -75.1611, 39.94671, -75.16259, false),
 
-VALUES ("blockThirtyOne", "Clinton St - 11th > 10th", 4, 39.94538, -75.15962, 39.9452, -75.15817, false)
-VALUES ("blockThirtyTwo", "Clinton St - 10th > 9th", 4, 39.94518, -75.15805, 39.945, -75.1566, false)
+       ("blockThirtyOne", "Clinton St - 11th > 10th", 4, 39.94538, -75.15962, 39.9452, -75.15817, false),
+       ("blockThirtyTwo", "Clinton St - 10th > 9th", 4, 39.94518, -75.15805, 39.945, -75.1566, false),
 
-VALUES ("blockThirtyThree", "Pine St - Broad > Juniper", 4, 39.94554, -75.16498, 39.94543, -75.16399, false)
-VALUES ("blockThirtyFour", "Pine St - Juniper > 13th", 4, 39.94542, -75.16392, 39.9453, -75.16297, false)
-VALUES ("blockThirtyFive", "Pine St - 13th > 12th", 4, 39.94529, -75.1629, 39.9451, -75.16141, false)
-VALUES ("blockThirtySix", "Pine St - 12th > 11th", 4, 39.94508, -75.16129, 39.94491, -75.15985, false)
-VALUES ("blockThirtySeven", "Pine St - 11th > 10th", 4, 39.94489, -75.15972, 39.9447, -75.15827, false)
-VALUES ("blockThirtyEight", "Pine St - 10th > 9th", 4, 39.94469, -75.15815, 39.94451, -75.1567, false)
-VALUES ("blockThirtyNine", "Pine St - 9th > 8th", 4, 39.9445, -75.15659, 39.94431, -75.15513, false)
-VALUES ("blockFourty", "Pine St - 8th > 7th", 4, 39.9443, -75.155, 39.94412, -75.15355, false)
+       ("blockThirtyThree", "Pine St - Broad > Juniper", 4, 39.94554, -75.16498, 39.94543, -75.16399, false),
+       ("blockThirtyFour", "Pine St - Juniper > 13th", 4, 39.94542, -75.16392, 39.9453, -75.16297, false),
+       ("blockThirtyFive", "Pine St - 13th > 12th", 4, 39.94529, -75.1629, 39.9451, -75.16141, false),
+       ("blockThirtySix", "Pine St - 12th > 11th", 4, 39.94508, -75.16129, 39.94491, -75.15985, false),
+       ("blockThirtySeven", "Pine St - 11th > 10th", 4, 39.94489, -75.15972, 39.9447, -75.15827, false),
+       ("blockThirtyEight", "Pine St - 10th > 9th", 4, 39.94469, -75.15815, 39.94451, -75.1567, false),
+       ("blockThirtyNine", "Pine St - 9th > 8th", 4, 39.9445, -75.15659, 39.94431, -75.15513, false),
+       ("blockFourty", "Pine St - 8th > 7th", 4, 39.9443, -75.155, 39.94412, -75.15355, false),
 
-VALUES ("blockFourtyOne", "Lombard St - 7th > 8th", 4, 39.94322, -75.15375, 39.94341, -75.15519, false)
-VALUES ("blockFourtyTwo", "Lombard St - 8th > 9th", 4, 39.94343, -75.15532, 39.94361, -75.15679, false)
-VALUES ("blockFourtyThree", "Lombard St - 9th > 10th", 4, 39.94362, -75.15692, 39.9438, -75.15834, false)
-VALUES ("blockFourtyFour", "Lombard St - 10th > 11th", 4, 39.94382, -75.15847, 39.944, -75.15992, false)
-VALUES ("blockFourtyFive", "Lombard St - 11th > 12th", 4, 39.94401, -75.16004, 39.9442, -75.16148, false)
-VALUES ("blockFourtySix", "Lombard St - 12th > 13th", 4, 39.94421,, -75.16161, 39.94439, -75.1631, false)
-VALUES ("blockFourtySeven", "Lombard St - 13th > Juniper", 4, 39.9444, -75.16317, 39.94452, -75.16411, false)
-VALUES ("blockFourtyEight", "Lombard St - Juniper > Broad", 4, 39.94453, -75.16419, 39.94465, -75.16518, false)
+       ("blockFourtyOne", "Lombard St - 7th > 8th", 4, 39.94322, -75.15375, 39.94341, -75.15519, false),
+       ("blockFourtyTwo", "Lombard St - 8th > 9th", 4, 39.94343, -75.15532, 39.94361, -75.15679, false),
+       ("blockFourtyThree", "Lombard St - 9th > 10th", 4, 39.94362, -75.15692, 39.9438, -75.15834, false),
+       ("blockFourtyFour", "Lombard St - 10th > 11th", 4, 39.94382, -75.15847, 39.944, -75.15992, false),
+       ("blockFourtyFive", "Lombard St - 11th > 12th", 4, 39.94401, -75.16004, 39.9442, -75.16148, false),
+       ("blockFourtySix", "Lombard St - 12th > 13th", 4, 39.94421, -75.16161, 39.94439, -75.1631, false),
+       ("blockFourtySeven", "Lombard St - 13th > Juniper", 4, 39.9444, -75.16317, 39.94452, -75.16411, false),
+       ("blockFourtyEight", "Lombard St - Juniper > Broad", 4, 39.94453, -75.16419, 39.94465, -75.16518, false),
 
-VALUES ("blockFourtyNine", "Rodman St - 12th > 11th", 4, 39.94369, -75.16159, 39.9435, -75.16015, false)
-VALUES ("blockFifty", "Rodman St - 8th > 7th", 4, 39.94279, -75.15533, 39.94261, -75.15388, false)
+       ("blockFourtyNine", "Rodman St - 12th > 11th", 4, 39.94369, -75.16159, 39.9435, -75.16015, false),
+       ("blockFifty", "Rodman St - 8th > 7th", 4, 39.94279, -75.15533, 39.94261, -75.15388, false),
 
-VALUES ("blockFiftyOne", "South St - 13th > 12th", 4, 39.94336, -75.16332, 39.94319, -75.16185, false)
-VALUES ("blockFiftyTwo", "South St - 12th > 11th", 4, 39.94317, -75.16171, 39.94299, -75.16027, false)
-VALUES ("blockFiftyThree", "South St - 10th > 9th", 4, 39.94278, -75.15857, 39.9426, -75.15713, false)
-VALUES ("blockFiftyFour", "South St - 9th > 8th", 4, 39.94258, -75.15701, 39.9424, -75.15555, false);
+       ("blockFiftyOne", "South St - 13th > 12th", 4, 39.94336, -75.16332, 39.94319, -75.16185, false),
+       ("blockFiftyTwo", "South St - 12th > 11th", 4, 39.94317, -75.16171, 39.94299, -75.16027, false),
+       ("blockFiftyThree", "South St - 10th > 9th", 4, 39.94278, -75.15857, 39.9426, -75.15713, false),
+       ("blockFiftyFour", "South St - 9th > 8th", 4, 39.94258, -75.15701, 39.9424, -75.15555, false);
 
 
